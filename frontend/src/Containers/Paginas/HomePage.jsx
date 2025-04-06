3ee5db01307daffdb2de8ab546dd3f5de4b02ffe
mohamedelabyad56 <mohamed.elabyad@prateducacio.com>
2025-04-06 23:14:12 +0200

    Mejorar la Pagina Horarios y crear la pagina contacto-ubicacion

--- frontend/src/Containers/Paginas/HomePage.jsx
+++ frontend/src/Containers/Paginas/HomePage.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import Layout from "../../hocs/layouts/layout"
 import HeroSection from "../../Componentes/Home/HeroSection "
 import SeccionFeatures from "../../Componentes/Home/Fatures"
+import Dividir from "../../Componentes/Home/div1"
 function Home() {
     useEffect(() => {
         window.scrollTo(0, 0)
@@ -11,9 +12,10 @@ function Home() {
     return (
         <Layout>
 
-           
+        <Dividir />
         <HeroSection />
         <SeccionFeatures />
+        <Dividir />
         </Layout>
     )
 }
