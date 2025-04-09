7952beccb288e7c243c5cc62583492edb054eb9b
Uriel27072005 <157824988+Uriel27072005@users.noreply.github.com>
2025-04-09 18:58:42 +0200

    Update Prot.lucesyenchufe.cpp
    pereparando codigos para cuando este el fisico

--- codes/Prot.lucesyenchufe.cpp
+++ codes/Prot.lucesyenchufe.cpp
@@ -1,44 +1,49 @@
-#define LUZ_PIN 3     // Pin para la luz inteligente (LED o relé)
-#define ENCHUFE_PIN 4 // Pin para el enchufe inteligente (relé)
-#define BTN_LUZ 5     // Botón para encender/apagar la luz
-#define BTN_ENCHUFE 6 // Botón para encender/apagar el enchufe
+#define LUZ_PIN        3  // Pin para la luz inteligente (LED o relé)
+#define ENCHUFE_PIN    4  // Pin para el enchufe inteligente (relé)
+#define BTN_LUZ        5  // Botón para encender/apagar la luz
+#define BTN_ENCHUFE    6  // Botón para encender/apagar el enchufe
 
-bool estado_luz = false;
-bool estado_enchufe = false;
-bool btnLuzPrevio = HIGH;
-bool btnEnchufePrevio = HIGH;
-unsigned long tiempoUltimoCambioLuz = 0;
-unsigned long tiempoUltimoCambioEnchufe = 0;
-const int debounceDelay = 50; // Tiempo de antirrebote en ms
+const unsigned long DEBOUNCE_DELAY = 50; // Tiempo de antirrebote en ms
+
+struct Dispositivo {
+    uint8_t pinSalida;
+    uint8_t pinBoton;
+    bool estado;
+    bool estadoPrevioBoton;
+    unsigned long ultimoCambio;
+    const char* nombre;
+};
+
+Dispositivo luz = {LUZ_PIN, BTN_LUZ, false, HIGH, 0, "LUZ"};
+Dispositivo enchufe = {ENCHUFE_PIN, BTN_ENCHUFE, false, HIGH, 0, "ENCHUFE"};
 
 void setup() {
-    pinMode(LUZ_PIN, OUTPUT);
-    pinMode(ENCHUFE_PIN, OUTPUT);
-    pinMode(BTN_LUZ, INPUT_PULLUP);
-    pinMode(BTN_ENCHUFE, INPUT_PULLUP);
+    pinMode(luz.pinSalida, OUTPUT);
+    pinMode(enchufe.pinSalida, OUTPUT);
+    pinMode(luz.pinBoton, INPUT_PULLUP);
+    pinMode(enchufe.pinBoton, INPUT_PULLUP);
 
     Serial.begin(9600);
     Serial.println("Sistema de luces y enchufes listo.");
 }
 
 void loop() {
-    unsigned long tiempoActual = millis(); // Capturar el tiempo actual
+    unsigned long tiempoActual = millis();
 
-    // Comprobación de botón de luz con debounce
-    if (digitalRead(BTN_LUZ) == LOW && btnLuzPrevio == HIGH && (tiempoActual - tiempoUltimoCambioLuz) > debounceDelay) {
-        estado_luz = !estado_luz;
-        digitalWrite(LUZ_PIN, estado_luz ? HIGH : LOW);
-        Serial.println(estado_luz ? "LUZ ENCENDIDA" : "LUZ APAGADA");
-        tiempoUltimoCambioLuz = tiempoActual;
-    }
-    btnLuzPrevio = digitalRead(BTN_LUZ);
+    manejarDispositivo(luz, tiempoActual);
+    manejarDispositivo(enchufe, tiempoActual);
+}
 
-    // Comprobación de botón de enchufe con debounce
-    if (digitalRead(BTN_ENCHUFE) == LOW && btnEnchufePrevio == HIGH && (tiempoActual - tiempoUltimoCambioEnchufe) > debounceDelay) {
-        estado_enchufe = !estado_enchufe;
-        digitalWrite(ENCHUFE_PIN, estado_enchufe ? HIGH : LOW);
-        Serial.println(estado_enchufe ? "ENCHUFE ACTIVADO" : "ENCHUFE DESACTIVADO");
-        tiempoUltimoCambioEnchufe = tiempoActual;
+void manejarDispositivo(Dispositivo& d, unsigned long tiempoActual) {
+    bool estadoBoton = digitalRead(d.pinBoton);
+
+    if (estadoBoton == LOW && d.estadoPrevioBoton == HIGH && (tiempoActual - d.ultimoCambio) > DEBOUNCE_DELAY) {
+        d.estado = !d.estado;
+        digitalWrite(d.pinSalida, d.estado ? HIGH : LOW);
+        Serial.print(d.nombre);
+        Serial.println(d.estado ? " ENCENDIDO" : " APAGADO");
+        d.ultimoCambio = tiempoActual;
     }
-    btnEnchufePrevio = digitalRead(BTN_ENCHUFE);
+
+    d.estadoPrevioBoton = estadoBoton;
 }
